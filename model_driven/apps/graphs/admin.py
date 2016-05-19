bf3247fe32f780ae656bad9c55f7234fbe1bada9
Songqing Liu <sliu@west.com>
2016-05-19 17:19:33 -0500

    Add model Module to admin template

--- model_driven/apps/graphs/admin.py
+++ model_driven/apps/graphs/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 
-from .models import Node, NodeType, Edge, EdgeType
+from .models import Node, NodeType, Edge, EdgeType, Module
 
 
-@admin.register(Node, NodeType, Edge, EdgeType)
+@admin.register(Node, NodeType, Edge, EdgeType, Module)
 class GraphsAdmin(admin.ModelAdmin):
     pass
