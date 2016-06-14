e40eaf18fc4a1c184772581fc5cd7cfbeedc12bb
Songqing Liu <sliu@west.com>
2016-06-14 14:38:21 -0500

    Add jenkins.sh

---
+++ build/jenkins.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+virtualenv --no-site-packages --clear env
+. /usr/local/virtualenvs/mdta/bin/activate
+
+pip install --download-cache /tmp/jenkins/pip-cache -r requirements/jenkins.txt
+
+python manage.py jenkins --enable-coverage --settings=mdta.settings.jenkins
