cb566f6d546380c5e9d8da76b6861b8704f02901
Songqing Liu <sliu@west.com>
2016-06-14 10:38:38 -0500

    Add deploy settings and jenkins settings, start creating unit tests etc.

---
+++ mdta/settings/qaci01.py
@@ -0,0 +1,20 @@
+__author__ = 'sliu'
+from mdta.settings.base import *
+
+SETTINGS_MODULE = 'mdta.settings.qaci01'
+
+ALLOWED_HOSTS = [
+    'apps.qaci01.wic.west.com',
+    'apps.qaci01'
+]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'mdta',
+        'USER': 'scorecard',
+        'PASSWORD': 'scorecard_development',
+        'HOST': 'qaci01.wic.west.com',
+        'PORT': '5432',
+    }
+}
