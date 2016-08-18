39966a1f2fc1900215e59225af4201a89fdc10e7
Songqing Liu <sliu@west.com>
2016-08-18 10:18:08 -0500

    Need use Postgres >9.4 to use JSON field

--- mdta/apps/testcases/views.py
+++ mdta/apps/testcases/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import get_object_or_404, render, redirect
 from testrail import APIClient, APIError
 
 from mdta.apps.projects.models import Project, Module, TestRailInstance, TestRailConfiguration
+from .models import TestCaseHistory
 from .utils import context_testcases, get_paths_through_all_edges, get_projects_from_testrail
 from .forms import TestrailConfigurationForm
 
@@ -32,10 +33,27 @@ def create_testcases(request, object_id):
         project = get_object_or_404(Project, pk=object_id)
         link_id = project.id
         testcases = create_routing_test_suite(project=project)
+
+        # print(testcases)
+        # # update TestCaseHistory for current project
+        # TestCaseHistory.objects.create(
+        #     project=project,
+        #     results=''
+        # )
+
     elif level == 'module':
         module = get_object_or_404(Module, pk=object_id)
         link_id = module.project.id
         testcases = create_routing_test_suite(modules=[module])
+
+        # print(testcases)
+        # try:
+        #     TestCaseHistory.objects.create(
+        #         project=module.project,
+        #         results=testcases
+        #     )
+        # except Exception as e:
+        #     print(str(e))
 
     context = context_testcases()
     context['testcases'] = testcases
