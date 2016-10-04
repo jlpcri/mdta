6f3e376ae420859f44a9a07c79f41858109abbb3
Songqing Liu <sliu@west.com>
2016-10-04 10:24:10 -0500

    QE-151, update property 'Outputs' of node 'Menu prompt/Menu prompt with confirm' from JSON to string to fulfill one outpt value.  Update Node add new Node&Edge completely works etc.

--- mdta/apps/graphs/templates/graphs/module/node_auto_edge_new.js
+++ mdta/apps/graphs/templates/graphs/module/node_auto_edge_new.js
@@ -27,7 +27,7 @@ function load_keys_from_type_contents_edge(item_id, location, type){
 
                 contents += '<tr id=\'{0}\'>'.format(rowCounter);
                 $.each(subkeys, function(k, v){
-                    contents += '<td><input name=\'{0}_{1}\' style=\'width:100%\' placeholder=\'JSON Format\'/></td>'.format(subkeys[k], rowCounter);
+                    contents += '<td><input name=\'edge_{0}_{1}\' style=\'width:100%\' placeholder=\'JSON Format\'/></td>'.format(subkeys[k], rowCounter);
                 });
                 contents += '</tr>';
 
@@ -37,7 +37,7 @@ function load_keys_from_type_contents_edge(item_id, location, type){
             } else {
                 contents += '<div class=\'row\' style=\'margin-top: 5px;\'>';
                 contents += '<div class=\'col-xs-3\'><label>{0}: </label></div>'.format(keys[k]);
-                contents += '<div class=\'col-xs-2\'><input name=\'{0}\'/></div>'.format(keys[k]);
+                contents += '<div class=\'col-xs-2\'><input name=\'edge_{0}\'/></div>'.format(keys[k]);
                 contents += '</div>';
             }
         });
@@ -75,7 +75,9 @@ $('.moduleNodeEdgeNew').on('submit', function(e){
         err_location = '#moduleNodeEdgeNewErrMessage',
         edge_type = $('.moduleNodeEdgeNew #id_edge-type option:selected').text(),
         edge_properties = $(e.currentTarget).find('#module-node-edge-new-edge-properties input'),
-        edge_properties_no_input = true;
+        edge_properties_no_input = true,
+        data_index_node = '',
+        data_index_edge = '';
 
     if (node_name == ''){
         showErrMsg(err_location, 'Node Name is empty.');
@@ -92,6 +94,100 @@ $('.moduleNodeEdgeNew').on('submit', function(e){
         showErrMsg(err_location, 'At least input on edge property');
         return false;
     }
+
+    $('#node-property-table tbody tr').each(function(){
+        data_index_node += this.id + ' ';
+    });
+    $('input[name="node_property_data_index"]').val(data_index_node);
+    $('#edge-property-table tbody tr').each(function(){
+        data_index_edge += this.id + ' ';
+    });
+    $('input[name="edge_property_data_index"]').val(data_index_edge);
 });
 
 /* End Module Node New Node & Edge Code */
+
+
+function load_keys_from_type_contents(item_id, location, type, call_from_node_edit){
+    $.getJSON("{% url 'graphs:get_keys_from_type' %}?id={0}&type={1}".format(item_id, type)).done(function(data){
+        var keys = data['keys'],
+            subkeys = data['subkeys'],
+            rowCounter = 0,
+            contents = '';
+        $.each(keys, function(k, v){
+            if ((keys[k].indexOf('Data') >= 0) ) {
+                contents += '<div class=\'row\' style=\'margin-top: 5px;\'>';
+                contents += '<div class=\'col-xs-3\'><label>{0}: </label></div>'.format(keys[k]);
+                contents += '</div>';
+
+                contents += '<div class=\'row\' style=\'margin-top: 5px;\'>';
+                contents += '<div class=\'col-xs-1\'></div>';
+                contents += '<div class=\'col-xs-11\'>';
+                contents += '<table class=\'table moduleNodeEditPropertyTable\' id=\'{0}-property-table\'>'.format(type);
+
+                contents += '<thead><tr>';
+                $.each(subkeys, function(k, v){
+                    contents += '<th class=\'col-xs-5\'>{0}</th>'.format(subkeys[k]);
+                });
+                if (keys[k].indexOf('InputData') >= 0) {
+                    contents += '<th class=\'col-xs-1\'><button id=\'buttonAddData\' class=\'btn btn-xs\' type=\'button\'>Add Data</button></th>';
+                } else {
+                    contents += '<th class=\'col-xs-1\'></th>';
+                }
+                contents += '</tr></thead>';
+
+                contents += '<tbody>';
+
+                contents += '<tr id=\'{0}\'>'.format(rowCounter);
+                if (call_from_node_edit) {
+                    $.each(subkeys, function (k, v) {
+                        contents += '<td><input name=\'{0}_{1}\' placeholder=\'JSON Format\'/></td>'.format(subkeys[k], rowCounter);
+                    });
+                } else {
+                    $.each(subkeys, function (k, v) {
+                        contents += '<td><input name=\'node_{0}_{1}\' style=\'width:100%\' placeholder=\'JSON Format\'/></td>'.format(subkeys[k], rowCounter);
+                    });
+                }
+                contents += '</tr>';
+
+                contents += '</tbody></table>';
+                contents += '</div>';
+                contents += '</div>';
+            } else {
+                contents += '<div class=\'row\' style=\'margin-top: 5px;\'>';
+                contents += '<div class=\'col-xs-3\'><label>{0}: </label></div>'.format(keys[k]);
+                contents += '<div class=\'col-xs-2\'><input name=\'node_{0}\'/></div>'.format(keys[k]);
+                contents += '</div>';
+            }
+        });
+        //console.log(contents)
+        $(location).html(contents);
+
+        $('#buttonAddData').click(function(){
+            rowCounter++;
+            node_property_add_data(subkeys, rowCounter, '.moduleNodeEditPropertyTable', call_from_node_edit);
+        });
+    });
+}
+
+function node_property_add_data(subkeys, rowCounter, location, call_from_node_edit){
+    var newRow = '';
+
+    newRow += '<tr id=\'{0}\'>'.format(rowCounter);
+    if (call_from_node_edit) {
+        $.each(subkeys, function (k, v) {
+            newRow += '<td><input name=\'{0}_{1}\' placeholder=\'JSON Format\'/></td>'.format(subkeys[k], rowCounter);
+        });
+    } else {
+        $.each(subkeys, function (k, v) {
+            newRow += '<td><input name=\'node_{0}_{1}\' style=\'width:100%\' placeholder=\'JSON Format\'/></td>'.format(subkeys[k], rowCounter);
+        });
+    }
+    newRow += '<td class=\'text-center\'><a href=\'#\' onclick=\'deleteRow(this);\'><i class=\'fa fa-trash-o fa-lg\'></i></a></td>';
+    newRow += '</tr>';
+    $(location).append(newRow)
+}
+
+function deleteRow(row){
+    $(row).closest('tr').remove();
+}
