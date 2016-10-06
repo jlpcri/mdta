d952be20e7cd0ad05c7cf6a5c025a43db637421a
Songqing Liu <sliu@west.com>
2016-10-06 12:09:04 -0500

    Update PreCondition edge to testcases

--- mdta/apps/testcases/utils_backwards_traverse.py
+++ mdta/apps/testcases/utils_backwards_traverse.py
@@ -5,7 +5,6 @@ CONSTRAINTS_TRUE_OR_FALSE = 'tof'
 
 
 def path_traverse_backwards(path):
-    data = {}
     tcs = []
     constraints = []
     path.reverse()
@@ -15,7 +14,8 @@ def path_traverse_backwards(path):
             if isinstance(step, Node):
                 if step.type.name in ['DataQueries Database', 'DataQueries WebService']:
                     result_found = get_data_node_result(step, constraints)
-                    constraints.append(result_found)
+                    if result_found:
+                        constraints.append(result_found)
                 else:
                     traverse_node(step, tcs, path[index + 1])
             elif isinstance(step, Edge):
@@ -23,6 +23,10 @@ def path_traverse_backwards(path):
                 if step.type.name == 'Data':
                     constraints += assert_current_edge_constraint(step)
                     constraints += assert_high_priority_edges_negative(step)
+
+                pre_condition = assert_precondition(step)
+                if pre_condition not in constraints:
+                    constraints += pre_condition
         else:
             traverse_node(step, tcs)
 
@@ -35,6 +39,84 @@ def path_traverse_backwards(path):
     return data
 
 
+def get_data_node_result(node, constraints):
+    dicts = node.properties[node.type.keys_data_name]
+    data = {}
+    compare_key = ''
+
+    if len(constraints) > 0:
+        for each in dicts:
+            found = True
+            for constraint in constraints:
+                for key in constraint.keys():
+                    if key != CONSTRAINTS_TRUE_OR_FALSE:
+                        compare_key = key
+                if constraint[CONSTRAINTS_TRUE_OR_FALSE] == 'True' \
+                        and each['Outputs'][compare_key] != constraint[compare_key]:
+                    found = False
+                    break
+                elif constraint[CONSTRAINTS_TRUE_OR_FALSE] == 'False' \
+                        and each['Outputs'][compare_key] == constraint[compare_key]:
+                    found = False
+                    break
+
+            if found:
+                data = each['Inputs']
+                break
+
+    return data
+
+
+def assert_current_edge_constraint(edge):
+    data = [get_edge_constraints(edge, rule='True')]
+
+    return data
+
+
+def assert_high_priority_edges_negative(edge):
+    data = []
+    edges = edge.from_node.leaving_edges.exclude(id=edge.id)
+    for each_edge in edges:
+        if each_edge.priority < edge.priority:
+            data.append(get_edge_constraints(each_edge, rule='False'))
+
+    return data
+
+
+def assert_precondition(edge):
+    data = []
+    edges = edge.from_node.leaving_edges
+    for each_edge in edges:
+        if each_edge.type.name == 'PreCondition':
+            tmp = each_edge.properties[each_edge.type.keys_data_name][each_edge.type.subkeys_data_name]
+            if each_edge.id == edge.id:
+                tmp['tof'] = 'True'
+            else:
+                tmp['tof'] = 'False'
+            data.append(tmp)
+            break
+
+    return data
+
+
+def get_edge_constraints(item, rule):
+    data = {}
+    for key in item.properties:
+        if key == 'OutputData':
+            try:
+                for constraint_key in item.properties[key][item.type.subkeys_data_name]:
+                    data = {
+                        constraint_key: item.properties[key][item.type.subkeys_data_name][constraint_key],
+                        CONSTRAINTS_TRUE_OR_FALSE: rule  # True or False
+                    }
+
+            except (KeyError, TypeError) as e:
+                print(e.messages)
+                pass
+    return data
+
+
+# --------------- Old methods Start, might removed in the future ---------------
 def add_step(step, tcs):
     """
     Add step to test cases
@@ -85,66 +167,6 @@ def node_prompt(node, preceding_edge=None):
     }
 
 
-def get_data_node_result(node, constraints):
-    dicts = node.properties[node.type.keys_data_name]
-    data = {}
-    compare_key = ''
-
-    for each in dicts:
-        found = True
-        for constraint in constraints:
-            for key in constraint.keys():
-                if key != CONSTRAINTS_TRUE_OR_FALSE:
-                    compare_key = key
-            if constraint[CONSTRAINTS_TRUE_OR_FALSE] == 'True' \
-                    and each['Outputs'][compare_key] != constraint[compare_key]:
-                found = False
-                break
-            elif constraint[CONSTRAINTS_TRUE_OR_FALSE] == 'False' \
-                    and each['Outputs'][compare_key] == constraint[compare_key]:
-                found = False
-                break
-
-        if found:
-            data = each['Inputs']
-            break
-
-    return data
-
-
-def assert_current_edge_constraint(edge):
-    data = [get_edge_constraints(edge, rule='True')]
-
-    return data
-
-
-def assert_high_priority_edges_negative(edge):
-    data = []
-    edges = edge.from_node.leaving_edges.exclude(id=edge.id)
-    for each_edge in edges:
-        if each_edge.priority < edge.priority:
-            data.append(get_edge_constraints(each_edge, rule='False'))
-
-    return data
-
-
-def get_edge_constraints(item, rule):
-    data = {}
-    for key in item.properties:
-        if key == 'OutputData':
-            try:
-                for constraint_key in item.properties[key][item.type.subkeys_data_name]:
-                    data = {
-                        constraint_key: item.properties[key][item.type.subkeys_data_name][constraint_key],
-                        CONSTRAINTS_TRUE_OR_FALSE: rule  # True or False
-                    }
-
-            except (KeyError, TypeError) as e:
-                print(e.messages)
-                pass
-    return data
-
-
 def get_item_properties(item):
     data = ''
     for key in item.properties:
@@ -166,3 +188,5 @@ def get_item_properties(item):
                 data += key
 
     return data
+
+# --------------- Old methods End, might removed in the future ---------------
