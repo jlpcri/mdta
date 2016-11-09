1e918f3d7b1570fa47544788be16999aa1ce0a97
Alex Heyden <caheyden@west.com>
2016-11-09 10:28:25 -0600

    Changed runner unit test to more accurately reflect current usage.

--- mdta/apps/runner/tests/test_hat_generation.py
+++ mdta/apps/runner/tests/test_hat_generation.py
@@ -5,12 +5,14 @@ from mdta.apps.runner.utils import HATScript
 
 class BasicConnectionToLab(TestCase):
     """
-    Ill-advised test that ensures we can connect to the QA lab and run a basic HAT script
-    on the current machine.
+    Ill-advised test that ensures we can connect to the QA lab and run a basic HAT script.
     """
     def test_startcall_recognition(self):
         hs = HATScript()
+        hs.remote_server = 'qaci01.wic.west.com'
+        hs.remote_user = 'wicqacip'
+        hs.remote_password = 'LogFiles'
         hs.basic_connection_test()
-        response = hs.hatit_execute()
-        self.assertEqual(200, response.status_code, 'test_startcall_recognition received a status code other than 200')
+        response = hs.remote_hat_execute()
+        print(hs.results())
 
