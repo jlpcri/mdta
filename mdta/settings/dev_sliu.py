b76a7070a7dc5716bcc300d6fc460fab5d22ce88
Songqing Liu <sliu@west.com>
2017-01-20 10:46:30 -0600

    Fix content render of intro template etc.

--- mdta/settings/dev_sliu.py
+++ mdta/settings/dev_sliu.py
@@ -6,7 +6,7 @@ INSTALLED_APPS += ['debug_toolbar', ]
 MIDDLEWARE_CLASSES += ['debug_toolbar.middleware.DebugToolbarMiddleware', ]
 INTERNAL_IPS = ['127.0.0.1', '10.6.20.127', '10.27.170.225']
 
-DATABASES = {
+DB_DEFAULT = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'mdta',
@@ -17,3 +17,27 @@ DATABASES = {
         'PORT': '5433'  # Version 9.4.4
     }
 }
+
+DB_AWS_PG = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'mdta',
+        'USER': 'ceeq',
+        'PASSWORD': 'ceeq_development',
+        'HOST': 'pginstance.ce8tamiymyr9.us-west-2.rds.amazonaws.com',
+        'PORT': '5432'
+    }
+}
+
+DB_AWS_SQLITE3 = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'mysqldb',
+        'USER': 'ceeq',
+        'PASSWORD': 'ceeq_development',
+        'HOST': 'mysqlinstance.ce8tamiymyr9.us-west-2.rds.amazonaws.com',
+        'PORT': '3306'
+    }
+}
+
+DATABASES = DB_DEFAULT
