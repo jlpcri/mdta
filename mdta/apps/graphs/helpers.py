7bb377b218fa0fe057582ce7e5ab90338318a95a
seenaomi <ngsee@west.com>
2017-02-09 17:23:29 -0600

    put all the parsing together. Working on matching them and bunching them in proper dictionaries.

--- mdta/apps/graphs/helpers.py
+++ mdta/apps/graphs/helpers.py
@@ -12,27 +12,22 @@ PROMPT_TEXT = "prompt text"
 STATE_NAME = "state name"
 
 
-
 @transaction.atomic
-def parse_out_module_names(vuid, project_id):
+def parse_out_modulesandnodes(vuid, project_id):
     df = pd.read_excel(vuid.file.path)
     df.columns = map(str.lower, df.columns)
     project = Project.objects.get(pk=project_id)
 
-    pgnames = (df[PAGE_NAME].unique())
-    for p in pgnames:
-        Module.objects.create(name=p, project=project)
-
-    return {"valid": True, "message": 'Module \'{0}\' is added to \'{1}\''.format(p, project)}
-
-
-@transaction.atomic
-def parse_out_node_names(vuid):
-    df = pd.read_excel(vuid.file.path)
-    df.columns = map(str.lower, df.columns)
     mylist = []
 
+    pgnames = (df[PAGE_NAME])
     pnames = (df[PROMPT_NAME])
+    ptext = (df[PROMPT_TEXT])
+    stnames = (df[STATE_NAME]).unique()
+
+    # for p in pgnames.unique():
+    #     Module.objects.create(name=p, project=project)
+
     if pnames.str.find('_').any() != -1:
         pnames = pnames.str.replace('_', ' ')
 
@@ -42,37 +37,29 @@ def parse_out_node_names(vuid):
         mylist.append(p.strip())
         mylist = list(OrderedSet(mylist))
 
+    mydict = {}
+    for x in range(len(df)):
+        currentid = df.iloc[x, 0]
+        currentvalue = df.iloc[x, 2]
+        mydict.setdefault(currentid, [])
+        mydict[currentid].append(currentvalue)
+
+    print(mydict)
+
     for my in mylist:
         print(my)
 
-    return {"valid": True, "message": 'Handled'}
-
-
-@transaction.atomic
-def parse_out_node_types(vuid):
-    df = pd.read_excel(vuid.file.path)
-    df.columns = map(str.lower, df.columns)
-    m = vuid.project.modules
-    print(m)
-    n = vuid.project.nodes
-    print(n)
-
-    stnames = (df[STATE_NAME]).unique()
     for st in stnames:
-
         print(st)
-
-    return {"valid": True, "message": 'Handled'}
-
-
-@transaction.atomic
-def parse_out_verbiage(vuid):
-    df = pd.read_excel(vuid.file.path)
-    df.columns = map(str.lower, df.columns)
-    ptext = (df[PROMPT_TEXT])
 
     for pt in ptext:
         print(pt)
+
+    # m = vuid.project.modules
+    # print(m)
+    #
+    # n = vuid.project.nodes
+    # print(n)
 
     return {"valid": True, "message": 'Handled'}
 
@@ -81,22 +68,7 @@ def upload_vuid(uploaded_file, user, project_id):
     vuid = VUID(filename=uploaded_file.name, file=uploaded_file, project_id=project_id, upload_by=user)
     vuid.save()
 
-    result = parse_out_module_names(vuid, project_id)
-    if not result['valid']:
-        vuid.delete()
-        return result
-
-    result = parse_out_node_names(vuid)
-    if not result['valid']:
-        vuid.delete()
-        return result
-
-    result = parse_out_verbiage(vuid)
-    if not result['valid']:
-        vuid.delete()
-        return result
-
-    result = parse_out_node_types(vuid)
+    result = parse_out_modulesandnodes(vuid, project_id)
     if not result['valid']:
         vuid.delete()
         return result
