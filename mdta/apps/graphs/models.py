c46b133285be1c17ca6cb597270577691b7f859d
Songqing Liu <sliu@west.com>
2017-02-22 12:20:23 -0600

    Model update QE-245

--- mdta/apps/graphs/models.py
+++ mdta/apps/graphs/models.py
@@ -21,6 +21,9 @@ class NodeType(models.Model):
     subkeys = ArrayField(models.CharField(max_length=50), null=True, blank=True,
                          verbose_name='SubKeys(Separated with comma)')
 
+    verbiage_keys = ArrayField(models.CharField(max_length=50), null=True, blank=True,
+                               verbose_name='VerbiageKeys(Separated with comma)')
+
     def __str__(self):
         return '{0}: {1}'.format(self.name, self.keys)
 
@@ -92,6 +95,9 @@ class Node(models.Model):
 
     # Property for the Node, Keys are from NodeType
     properties = JSONField(null=True, blank=True)
+
+    # Verbiage for the PromptNode, Verbiage_Keys are from NodeType
+    verbiage = JSONField(null=True, blank=True)
 
     class Meta:
         unique_together = ('module', 'name',)
