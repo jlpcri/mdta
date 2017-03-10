1dd1c45a8b36038dbd6c108e4a79eca155324d14
vparihar <devnull@localhost>
2017-03-10 13:59:26 -0600

    Added Unit Test for graph view testing

---
+++ mdta/apps/graphs/tests/test_graphs_view.py
@@ -0,0 +1,28 @@
+from django.test import TestCase, Client
+from django.core.urlresolvers import reverse
+from mdta.apps.users.models import HumanResource
+import mdta.apps.graphs.views import landing
+
+class GraphsViewsTest(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.user_account = {
+            'username': 'UserAccount',
+            'password': 'UserPassword'
+        }
+        self.user = User.objects.create_user(
+            username=self.user_account['username'],
+            password=self.user_account['password']
+        )
+        self.hr = HumanResource.objects.create(
+            user=self.user
+        )
+    def test_landing_return_200(self):
+        response = self.client.get(reverse('graphs'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_login_required(self):
+        response = self.client.get(reverse('landing'))
+        self.assertContains(response, 'Please use your Active Directory credentials.')
+
