be233ab460f9946f933ff07189a5cb0272d3e90b
seenaomi <ngsee@west.com>
2017-03-14 14:02:22 -0500

    a little uncommited clean up.

--- mdta/settings/dev_ngsee.py
+++ mdta/settings/dev_ngsee.py
@@ -7,7 +7,7 @@ INSTALLED_APPS += ['debug_toolbar', ]
 MIDDLEWARE_CLASSES += ['debug_toolbar.middleware.DebugToolbarMiddleware', ]
 INTERNAL_IPS = ['127.0.0.1', '10.6.20.97', '10.6.20.60']
 
-DATABASES = {
+QACI = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'mdta',
@@ -30,4 +30,4 @@ DB_6437 = {
     }
 }
 
-DATABASES = DB_6437
+DATABASES = QACI
