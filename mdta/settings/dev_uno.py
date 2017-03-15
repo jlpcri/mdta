ec38e4fdef130374dd4dd37ba3b686f8a718ae3a
Songqing Liu <sliu@west.com>
2017-03-15 13:58:59 -0500

    Add branch settings file

---
+++ mdta/settings/dev_uno.py
@@ -0,0 +1,50 @@
+__author__ = 'sliu'
+from mdta.settings.base import *
+
+DEBUG = True
+
+DB_QACI01 = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'mdta',
+        'USER': 'scorecard',
+        'PASSWORD': 'scorecard_development',
+        'HOST': 'qaci01.wic.west.com',
+        # 'PORT': '5432', #  Version 9.1.10
+        'PORT': '5433'  # Version 9.4.4
+    }
+}
+
+DB_6437 = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'mdta',
+        'USER': 'ceeq',
+        'PASSWORD': 'FkhfDhPx%A=-?h_snCMuQ$&%5crcx%tpxw24pVVp+U-UrXs4q6=uK=8^-evN-RxA',
+        'HOST': 'linux6437.wic.west.com',
+        'PORT': '5433'   # posgtres 9.4 instance
+    }
+}
+
+DB_AWS_PG = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'mdta',
+        'USER': 'ceeq',
+        'PASSWORD': 'ceeq_development',
+        'HOST': 'wicqacip.ce8tamiymyr9.us-west-2.rds.amazonaws.com',
+        'PORT': '5432'
+    }
+}
+
+DB_DOCKER = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'HOST': 'db',
+        'PORT': '5432'
+    }
+}
+
+DATABASES = DB_DOCKER
