3886ca66f3127304d6b258599ffc5dae238a0cff
Songqing Liu <sliu@west.com>
2017-03-22 14:23:56 -0500

    Add integration of Celery with RabbitMQ to Docker

--- mdta/celery_module.py
+++ mdta/celery_module.py
@@ -10,8 +10,8 @@ elif socket.gethostname() == "seenaomi-HP-Compaq-6005-Pro-SFF-PC":
 elif socket.gethostname() == 'qaci01':
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mdta.settings.qaci01')
 else:
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mdta.settings.base')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mdta.settings.dev_uno')
 
-app = Celery('mdta')
+app = Celery('mdta', broker='amqp://admin:mypass@rabbit//')
 app.config_from_object('mdta.celery_config')
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS, related_name='tasks')
