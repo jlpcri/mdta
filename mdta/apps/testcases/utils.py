58c0ea5faaa998432bc8f7fd8119cbd9f858cec8
Songqing Liu <sliu@west.com>
2017-05-09 10:04:38 -0500

    Update route strategy on MenuPromptWithConfirmation node

--- mdta/apps/testcases/utils.py
+++ mdta/apps/testcases/utils.py
@@ -125,11 +125,12 @@ def get_paths_through_all_edges(edges, th_module=None, language=None, shortest_s
                                 # print(exist_edge)
                             if exist_node and not exist_node['visited']:
                                 negative_testcase_generation(data, path_data, title, NEGATIVE_TESTS_LIST, edge, language=language)
-                                exist_node['visited'] = True
                                 # print(exist_node)
-                            if edge.to_node.type.name == NODE_MP_NAME[1]:
-                                negative_testcase_generation(data, path_data, title, NEGATIVE_CONFIRM_TESTS_LIST, edge, language=language)
-                                rejected_testcase_generation(data, path_data, title, edge.to_node, edge, language=language)
+                                if edge.to_node.type.name == NODE_MP_NAME[1]:
+                                    negative_testcase_generation(data, path_data, title, NEGATIVE_CONFIRM_TESTS_LIST, edge, language=language)
+                                    rejected_testcase_generation(data, path_data, title, edge.to_node, edge, language=language)
+
+                                exist_node['visited'] = True
         # print(domain_set)
     else:
         for edge in edges:
