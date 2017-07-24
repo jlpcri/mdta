75b029c380911e8369db466c788bfb938e021646
Songqing Liu <sliu@west.com>
2017-07-24 13:15:28 -0500

    Add project delete method

--- mdta/apps/help/views.py
+++ mdta/apps/help/views.py
@@ -2,6 +2,9 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from django.conf import settings
 
+
 @login_required
 def help(request):
+    if request.user.username == 'sliu':
+        return render(request, 'help/help.html')
     return redirect(settings.CONFLUENCE_LINK)
