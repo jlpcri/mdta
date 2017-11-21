c168019bf79ebff8b7b9062afcc2e8fc8ae2c386
Songqing Liu <sliu@west.com>
2017-11-21 16:40:48 -0600

    Update DB settings on qaci01

--- mdta/settings/qaci01.py
+++ mdta/settings/qaci01.py
@@ -8,13 +8,24 @@ ALLOWED_HOSTS = [
     'apps.qaci01'
 ]
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'mdta',
+#         'USER': 'scorecard',
+#         'PASSWORD': 'scorecard_development',
+#         'HOST': 'qaci01.wic.west.com',
+#         'PORT': '5433'  # Version 9.4.4
+#     }
+# }
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'mdta',
-        'USER': 'scorecard',
-        'PASSWORD': 'scorecard_development',
-        'HOST': 'qaci01.wic.west.com',
-        'PORT': '5433'  # Version 9.4.4
+        'USER': 'ceeq',
+        'PASSWORD': 'FkhfDhPx%A=-?h_snCMuQ$&%5crcx%tpxw24pVVp+U-UrXs4q6=uK=8^-evN-RxA',
+        'HOST': 'linux6437.wic.west.com',
+        'PORT': '5433'   # posgtres 9.4 instance
     }
 }
